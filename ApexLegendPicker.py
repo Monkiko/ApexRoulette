f7ed21bf2489bc39bcceddeecbd6a2beaf97050b
Monkiko <Monkiko425@gmail.com>
2021-06-02 15:33:51 -0500

    Added image and initial Picker scripts

---
+++ ApexLegendPicker.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import random
+
+Legends = ['Bloodhound', 'Bangalore', 'Caustic', 'Crypto', 'Fuse', 'Gibraltar', 'Horizon', 'Lifeline', 'Loba', 'Mirage',
+            'Octane', 'Pathfinder', 'Rampart', 'Revenant', 'Valkyrie', 'Wattson', 'Wraith']
+
+print(random.choice(Legends))
